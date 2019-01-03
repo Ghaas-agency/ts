bd68b3d8a00517db845f2812b99ebef651a330d3
Akshay Dhiman <akshay.virgo@gmail.com>
2019-01-03 12:17:18 +0530

    design fixes

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -5,13 +5,25 @@ var header = document.getElementById('header');
 var links = document.getElementsByClassName('menu-link');
 var menuIcon = document.getElementsByClassName('header-menu-icon')[0];
 var tsLogo = document.getElementsByClassName('header-logo')[0];
+var headerList = document.getElementById('header-list');
 
 tsLogo.style.filter = "grayscale(100%) brightness(4)";
 
+// Set initial styles for the header if no scroll event is fired.
+if (window.pageYOffset > 10) {
+  header.style.backgroundColor = "rgba(255,255,255,100%)";
+  header.style.boxShadow = "rgba(0, 0, 0, 0.2) 0px 3px 3px";
+  menuIcon.style.filter = "invert(100%)";
+  for (var i = 0; i < links.length; i++) {
+    links[i].style.color = '#3a3a3a';
+  }
+}
+
 window.onscroll = function() {
+  // Setting styles to the header.
   if (window.pageYOffset > 10 && window.pageYOffset < 101) {
     header.style.backgroundColor = "rgba(255,255,255," + window.pageYOffset + "%)";
-    header.style.boxShadow = "rgba(0, 0, 0, " + (0.4 * window.pageYOffset / 100) + ") 0px 3px 3px";
+    header.style.boxShadow = "rgba(0, 0, 0, " + (0.2 * window.pageYOffset / 100) + ") 0px 3px 3px";
     for (var i = 0; i < links.length; i++) {
       links[i].style.color = '#3a3a3a';
     }
@@ -19,7 +31,7 @@ window.onscroll = function() {
     tsLogo.style.filter = "unset";
   } else if (window.pageYOffset > 100) {
     header.style.backgroundColor = "rgba(255,255,255,100%)";
-    header.style.boxShadow = "rgba(0, 0, 0, 0.4) 0px 3px 3px";
+    header.style.boxShadow = "rgba(0, 0, 0, 0.2) 0px 3px 3px";
     menuIcon.style.filter = "invert(100%)";
   } else {
     tsLogo.style.filter = "grayscale(100%) brightness(4)";
@@ -31,8 +43,6 @@ window.onscroll = function() {
     }
   }
 }
-
-var headerList = document.getElementById('header-list');
 
 function matchMediaQuery(mm) {
   if (mm.matches) {
@@ -49,6 +59,7 @@ mm.addListener(matchMediaQuery);
 
 var menuToggle = true;
 
+// Add event to menu for mobile.
 window.onclick = function(e) {
   if (e.target.className === 'header-menu-icon') {
     if (menuToggle) {
